8e63bc8dd560ea680fd5730b8545dc6f849250c0
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2023-08-27 16:20:19 +0530

    first commit

---
+++ backend/middleware/Auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const TOKEN = process.env.REACT_APP_TOKEN_KEY;
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'].split(' ')[1];
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    jwt.verify(token, TOKEN ,(err,decode)=>{
+     if(err){console.log('yahi pe hai error',err);}
+     else{
+          req.user = decode;
+          return next();
+     }
+    });
+  } catch (err) {
+     console.log(err);
+    return res.status(401).send("Invalid Token");
+  }
+};
+
+
+// token generator //
+
+module.exports = verifyToken;
